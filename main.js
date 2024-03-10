326e74610d06ae4927e14283c480dad1fd899d1a
k <developerlyk@gmail.com>
2024-03-10 15:45:27 +0900

    for-each,map,filter,reduce

--- main.js
+++ main.js
@@ -1,47 +1,30 @@
-// == 데이터타입 비교 안함 값만 같으면 true ===데이터타입까지 비교
-//if 조건문
-const num = 10;
-if (num < 10) {
-  console.log("10보다 작습니다.");
-} else if (num === 10) {
-  console.log("같다");
-} else {
-  console.log("작다");
-}
-
-//3항연산자 조건?참 실행:거짓 실행;
-num > 10 ? true : false;
-
-//스위치 케이스문
-const animal = "lion";
-switch (animal) {
-  case "lion":
-    console.log("사자입니다");
-    break;
-  case "monkey":
-    console.log("원숭이입니다");
-    break;
-  default:
-    console.log("동물이 아닙니다");
-    break;
-}
-
-//반복문1
-const animals = [
-  { name: "lion", size: "big" },
-  { name: "monkey", size: "medium" },
-  { name: "dog", size: "small" },
+const coronaCases = [
+  { name: "Seoul", case: 1000 },
+  { name: "Daejeon", case: 1200 },
+  { name: "Daegu", case: 1400 },
+  { name: "Busan", case: 1500 },
+  { name: "Gwangju", case: 1100 },
 ];
-for (let i = 0; i < animals.length; i++) {
-  console.log(animals[i]);
-}
-//반복문2
-for (let animal of animals) {
-  console.log(animal.name);
-}
-//while문
-let i = 0;
-while (i < 10) {
-  console.log(i);
-  i++;
-}
+
+//forEach, map, filter, reduce
+//foreach(index도 출력가능)
+coronaCases.forEach((corona, index) => {
+  console.log(corona, index);
+});
+//map - 배열을 가공하여 새로운 형태의 배열을 만들때
+//name만으로 새로운 배열
+const coronaCities = coronaCases.map((corona) => {
+  return corona.name;
+});
+//filter - 배열에서 특정한 값을 가진 데이터를 필터링
+const dangerousCity = coronaCases.filter(function (corona, index) {
+  return corona.case > 1200; //조건에 맞는 데이터만 출력
+});
+console.log(dangerousCity);
+
+//reduce - 배열 값들을 합치거나 더한 새로운 값으로 재가공 할때
+//처음에 object가 들어가는 게 싫다면,0으로 기본 값 저장
+const totalCases = coronaCases.reduce((acc, cur) => {
+  return acc + cur.case;
+}, 0);
+console.log(totalCases);
