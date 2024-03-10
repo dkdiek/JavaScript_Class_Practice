3087478b3f1e2e8700540c0d3b2372d05e08b637
k <developerlyk@gmail.com>
2024-03-10 14:58:28 +0900

    function

--- main.js
+++ main.js
@@ -1,85 +1,51 @@
-console.log("hello");
+const item1 = 3000;
+const item2 = 2000;
+const item3 = 5000;
+const loyalty = 1000;
 
-//var, let, const
+console.log(showPrice(item1));
+showPrice(item2);
+showPrice(item3);
 
-//var는 전역으로 사용할 수 있음 안쓰는게 좋다
-for (var i = 0; i < 10; i++) {
-  var name = "scalper";
+// function
+/* 이 방식은 함수 선언식입니다. 함수 이름(showPrice)이 주어지고,
+그 다음에 매개변수(item)가 따라옵니다. 이 함수는 호출될 때마다 새로운 showPrice 함수 인스턴스가 생성됩니다.
+이 방식은 this 키워드가 동적으로 바인딩되며, 자체적으로 this 값을 가지게 됩니다.
+이 방식은 일반적으로 객체의 메서드로 사용될 때 유용합니다. 
+window에 등록된다. 함수가 선언되기 전 위에 작성해도 작동한다.
+*/
+function showPrice(item) {
+  return item + loyalty;
 }
-console.log(name);
-
-//let은 변수가 빈번하게 바뀌어야 할때
-//const는 상수 값 변경 불가 예측 가능
-//let과 const는 블럭 밖에서 사용불가
-
-//데이터 타입
-//String, Number, boolean, null, undefined
-
-const isMale = false;
-const age = 30;
-const name2 = "scalper";
-let money = null;
-let girlFriend = undefined;
-
-console.log(typeof name2);
-//표현식 사용
-console.log("my name is %s and i am %d", name2, age);
-//백틱사용 템플릿 문법
-console.log(`my name is ${name} and i am ${age}`);
-//문자열 자르기
-const animal = "tiger";
-//0번째부터 두 글자 0,1
-console.log(animal.substring(0, 2));
-
-//기준으로 짜르기 배열로 출력
-let fruits = "사과, 딸기, 배";
-fruits = fruits.split(", ");
-//콤마로 배열에 넣고 2번째 즉 배 출력
-console.log(fruits[2]);
-
-//array(js는 여러 데이터타입 혼합으로 배열 생성 가능)
-const animals = ["lion", "rabbit", "monkey"];
-//특정 인덱스에 추가
-animals[3] = "tiger";
-//마지막에 추가
-animals.push("tiger");
-//첫번째에 추가
-animals.unshift("dog");
-//마지막 하나 빼기
-animals.pop();
-//범위 지정해서 빼기
-//0번부터 3개 스플라이스(시작인덱스,갯수)
-animals.splice(0, 3);
-//slice(시작인덱스,끝인덱스) 슬라이스는 원본에 수정이 아니고 결과 값을 반환해준다
-animals.slice(0, 2);
-
-//배열인지 확인
-console.log(Array.isArray(animals));
-//배열의 인덱스 찾기
-console.log(animals.indexOf("monkey"));
-
-//object 속성과 값 쌍을 이루는 데이터(내부에 배열, 오브젝트 등 다양하게 추가가능)
-const monkey = {
-  name: "kiki",
-  age: 5,
-  weight: 40,
-  gender: "male",
-  isCute: true,
-  food: ["banana", "carrot"],
-  home: {
-    location: "zoo",
-    size: "big",
-    price: 3000,
+//es6 화살표 함수
+/* 이 방식은 ES6에서 도입된 화살표 함수입니다. 화살표 함수는 function 키워드 대신 =>를 사용하여 정의됩니다.
+이 방식은 함수 표현식입니다. 변수(showPrice)에 함수가 할당됩니다. 화살표 함수는 항상 익명입니다.
+화살표 함수는 자신의 this 값을 갖지 않고, 상위 스코프의 this 값을 그대로 사용합니다.
+이는 화살표 함수가 생성될 때의 스코프에서 this 값을 가져와서 사용한다는 의미입니다.
+window에 등록X. 함수가 선언되기 전 위에 작성하면 초기화 에러가 난다.
+렉시컬 스코프
+ */
+const showPrice2 = (item) => {
+  return item + loyalty;
+};
+//
+const obj = {
+  name: "test",
+  test1: function () {
+    console.log(this);
+  },
+  test2: () => {
+    console.log(this);
   },
 };
-//object 값 빼는 방법
-console.log(monkey["name"]);
-console.log(monkey.weight);
-//object 값 변경
-monkey.gender = "female";
 
-//json -데이터를 전송할때 문자열 형태로 보낼때
-const monkeyJson = JSON.stringify(monkey); //object를 json화
-console.log(monkeyJson);
-const parseJson = JSON.parse(monkeyJson); // json을 object 형태로 parsing
-console.log(parseJson);
+//this가 객체 자신을 가리키고
+obj.test1();
+//화살표 함수는 this가 윈도우를 가리킨다
+obj.test2();
+
+//아래처럼 하면 둘다 윈도우를 가리킨다 즉 화살표 함수는 항상 고정된 스코프를 가진다
+const a = obj.test1;
+const b = obj.test2;
+a();
+b();
